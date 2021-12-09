802a5c9059901251f732adc8165f2d3c53cf6f9a
harikrishnathatipelli <harikrishnat2112@gmail.com>
2021-12-09 19:54:11 +0530

    adding roboshop directory

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -3,6 +3,7 @@
 read -p "enter your age: " age
   if [ -z "${age}" ]; then
     echo inut is missing
+    exit
   fi
 
 
@@ -14,3 +15,8 @@ read -p "enter your age: " age
 
 
   ##Note: when u use variables in expression or in IF statement,always double quote them
+# Expressions can be can be combined using
+#Logical AND->  -a
+#[ exp1 -a exp2 ] if both are true then answer is true,otherwise false.
+#Logical OR->   -o
+#[ exp1 -o exp2 ] if any exp is true ,then it is true
---
+++ roboshop/roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USER_ID=${id -u}
+if [ ${USER_ID -ne 0} ]; then
+  echo -e "\e[1;31myou should be root user to perform the script\e[0m"
+  exit
+fi
+
+component=$1
+if [ -z "$commponent" ]; then
+  echo -e "\e[1;31mcompoent input missing\e[0m"
+  exit
+fi
+
+if [ ! -e components/${component}.sh ]; then
+  echo -e "\e[1;31mgiven component does not exit\e[0m"
+  exit
+fi
+bash components/${component}.sh
