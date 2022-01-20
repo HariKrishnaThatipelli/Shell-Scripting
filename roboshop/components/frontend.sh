6523b8dd1d38ca17694d0317ac87a1b356d4a49c
harikrishnathatipelli <harikrishnat2112@gmail.com>
2022-01-20 23:34:29 +0530

    adding roboshop directory

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -9,7 +9,7 @@ fi
 #Let's download the HTML content that serves the RoboSHop Project UI and deploy under the Nginx path.
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-Deploy in Nginx Default Location.
+#Deploy in Nginx Default Location.
 
 cd /usr/share/nginx/html
 rm -rf *
